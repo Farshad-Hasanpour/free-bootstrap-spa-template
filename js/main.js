3bed6d71c745796dd91b8ab61aa201cf989eeef9
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-10-07 13:25:20 +0330

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
-	numberOfPlants = 46;
+	const numberOfPlants = 46;
 	$('.page1').animate({opacity: '1'}, 1000);
-	initialfunction();
+	loadPage(true);
+	cardsContainerResize();
+	contentMinHeight();
+	gotTopButton();
+
+	function scrollToTop(){
+		$('html,body').animate({scrollTop:0}, 1000);
+	}
 	
 	$(window).resize(function(){
 		cardsContainerResize();
-		fluidContainer();
 		lastCardSize();
 		contentMinHeight();
 	});
 
 	$(window).scroll(gotTopButton);
 
-
 	$('.card-img-top').on('load', function(){
 		$(this).removeClass('loading');
 	});
 
-	$('#side-contact-us-button, #top-contact-us-button').on('click',function(event){
+	$('#side-contact-us-button, #top-contact-us-button').on('click',function(){
 		$('html, body').animate({
             scrollTop: $("#contact-us").offset().top
         }, 1000);
@@ -39,19 +44,16 @@ $(document).ready(function(){
 
 	$('.nav-link:not(.contact-us-button)').on('click', loadPage);
 
-	$('#go-top-button').on('click',function(){
-		$('html,body').animate({scrollTop:0}, 1000);
-	});
+	$('#go-top-button').on('click', scrollToTop);
 
-	$('#content').on('click', '.card-title a, #next-plant, #prev-plant',function(){
+	$('#content').on('click', '.card-title a, #next-plant, #prev-plant', function(){
 		// Number 0 makes scroll to top when page is reloaded or ajax is called
-		history.replaceState({}, 'گروه یاسمن', '?number=0');
+		history.replaceState({}, 'قالب رایگان', '?number=0');
 		
 		var plantId = $(this).attr('data-plant-id');
-		if(plantId < 1 || plantId > numberOfPlants){
-			return;
-		}
-		plantIndex = "plant" + plantId;
+		if(plantId < 1 || plantId > numberOfPlants) return;
+
+		const plantIndex = "plant" + plantId;
 		//TODO: Read data from json based on id
 		var image_source = plants[plantIndex]["image_source"] || "image/transparent-brand.png";
 		var prev = plants[plantIndex]["prev"] || 0;
@@ -63,9 +65,9 @@ $(document).ready(function(){
 		var application = plants[plantIndex]["application"] || "";
 
 		$.get('ajax/plant-details.html', function(data){
-  			// Remove all childs of #content except #more button
-  			$("#content > :not('#more')").remove();
-  			$('#content #more').css({display:'none'});
+  			// Remove all children of #content except #more button
+  			$("#content > *:not(#more)").remove();
+  			$('#content #more').css({display: 'none'});
   			
   			// Put data in plant-detail.html
   			data = $.parseHTML(data);
@@ -84,8 +86,8 @@ $(document).ready(function(){
   		});
 
 		// scroll to top
-		if($(this).attr('data-scroll-top') != 'no'){
-			$('html,body').animate({scrollTop:0}, 1000);
+		if($(this).attr('data-scroll-top') !== 'no'){
+			scrollToTop();
 		}
   		
   		// Force active plants nav-link
@@ -99,19 +101,16 @@ $(document).ready(function(){
   			// decide whether Load More Button should be displayed or not
 
   			// Remove all childs of #content except #more button
-  			$("#content > :not('#more')").remove();
+  			$("#content > :not(#more)").remove();
   			
   			// Add data to #content before #more button
   			$(data).insertBefore("#content #more");
   			$('#content #more').css({display:'flex'});
   			$('.page1').animate({opacity: '1'}, 1000);
   		});
-  		history.replaceState({}, 'گروه یاسمن', '?');
-  		$('html,body').animate({scrollTop:0}, 1000);
+  		history.replaceState({}, 'قالب رایگان', '?');
+		scrollToTop();
 	});
-
-
-	
 
 	function cardsContainerResize(){
 		if(window.innerWidth <= 768/* || (window.innerWidth > 768 && window.innerHeight < 380)*/){
@@ -120,15 +119,6 @@ $(document).ready(function(){
 			$('.cards-container').addClass('col-10').removeClass('col-12');
 		}
 	}
-
-	function fluidContainer(){
-		if(window.innerWidth > 1367){
-			$('#container').addClass('container').removeClass('container-fluid');
-		}else{
-			$('#container').addClass('container-fluid').removeClass('container');
-		}
-	}
-
 
 	function isPageVisible(number){
 		var pageClass = '.page1';
@@ -142,16 +132,12 @@ $(document).ready(function(){
 			return 'page not found';
 		}
 
-		if ($('#content '+pageClass).is(':visible')){
-			return true;
-		}
-
+		if ($('#content ' + pageClass).is(':visible')) return true;
 		return false;
-
 	}
 
 	function makePageVisible(number){
-		var pageClass = 'page1';
+		let pageClass = 'page1';
 		if(number == 2){
 			pageClass = 'page2';
 		}else if(number == 3){
@@ -163,30 +149,25 @@ $(document).ready(function(){
 		}
 		
 		// Get plants page using ajax
-		$.get('ajax/plants-'+pageClass+'.html', function(data){
+		$.get('ajax/plants-' + pageClass+'.html', function(data){
 			$(data).insertBefore("#content #more");
-			$('.'+pageClass).css({display:"flex"});
-			$('.'+pageClass).animate({opacity: '1'}, 1000);
+			$('.' + pageClass).css({display:"flex"});
+			$('.' + pageClass).animate({opacity: '1'}, 1000);
 			//Set size of last card
 			lastCardSize();
 			// Edit URL without loading it
-			var number = pageClass.substring(4, 5);
-			window.history.replaceState({}, 'گروه یاسمن', '?number='+number);
+			window.history.replaceState({}, 'قالب رایگان', '?number=' + pageClass.substring(4, 5));
 		});
-		
 	}
 
-	// This function is copied from stackoverflow
-	// https://stackoverflow.com/a/21903119/6810012
 	function getUrlParameter(sParam) {
-	    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+	    let sPageURL = decodeURIComponent(window.location.search.substring(1)),
 	        sURLVariables = sPageURL.split('&'),
 	        sParameterName,
 	        i;
 
 	    for (i = 0; i < sURLVariables.length; i++) {
 	        sParameterName = sURLVariables[i].split('=');
-
 	        if (sParameterName[0] === sParam) {
 	            return sParameterName[1] === undefined ? true : sParameterName[1];
 	        }
@@ -194,40 +175,30 @@ $(document).ready(function(){
 	}
 
 	function contentMinHeight(){
-		var height = window.innerHeight;
-		$('#content').css({'min-height':height });
+		$('#content').css({'min-height': window.innerHeight });
 	}
 
 	function gotTopButton(){
-		var button = $('#go-top-button');
-		var screenHeight = window.innerHeight / 1.5;
-		if($(this).scrollTop() > screenHeight){
-			$('#go-top-button').show();
-		}else{
-			$('#go-top-button').hide();
-		}
+		const button = $('#go-top-button');
+		if($(this).scrollTop() > (window.innerHeight / 1.5)) button.show();
+		else button.hide();
 	}
 
 	function loadPage(redirect = false){
-		var pageButtonId;
-		var topPageButtonId;
-		var $e;
-		var $e_other; // second nav
-		var pageName;
+		let $e;
+		let $e_other; // second nav
+		let pageName;
 
-		// true means url information is important
-		if(redirect == true){
-			pageButtonId = "#"+getUrlParameter('page')+"-link";
-			topPageButtonId = "#top-"+getUrlParameter('page')+"-link";
-			$e = $(pageButtonId);
-			$e_other = $(topPageButtonId);
+		if(redirect){
+			$e = $("#" + getUrlParameter('page')+"-link");
+			$e_other = $("#top-" + getUrlParameter('page') + "-link");
 			pageName = $e.attr('data-page');
 		}else{
 			$e = $(this);
 			pageName = $e.attr('data-page');
 
 			// Find other (hidden) nav
-			if($(this).attr('id').substring(0,3) == 'top' ){
+			if($(this).attr('id').substring(0, 3) === 'top' ){
 				// other nav is side nav
 				$e_other = $('.nav .nav-item .nav-link[data-page="'+ pageName +'"]');
 			}else{
@@ -237,55 +208,41 @@ $(document).ready(function(){
 		}
 
 		// if pageName is not provided OR clickep page is currently active
-		var currentAvtive = $e.closest('.nav, .navbar-nav').find('.active');
-		var pageToActivate = $e;
+		let currentAvtive = $e.closest('.nav, .navbar-nav').find('.active');
+		let pageToActivate = $e;
 
 		// other (hidden) nav
-		var current = $e_other.closest('.nav, .navbar-nav').find('.active');
-		var activate = $e_other;
+		let current = $e_other.closest('.nav, .navbar-nav').find('.active');
+		let activate = $e_other;
 
 		if(!pageName){
-			var number = getUrlParameter('number');
+			const number = getUrlParameter('number');
 			// if page is reloaded in sections more than 1
 			if(number == 0 || number == 2 || number == 3 || number == 4){
-				history.replaceState({}, 'گروه یاسمن', '?');
-				$('html,body').animate({scrollTop:0}, 1000);
+				history.replaceState({}, 'قالب رایگان', '?');
+				scrollToTop();
 			}
 			//cancel event
 			return;
 		}else if(currentAvtive.attr('data-page') == pageToActivate.attr('data-page')){
-			//Scroll to top
-			$('html,body').animate({scrollTop:0}, 1000);
-
+			scrollToTop();
 			// Edit browse Url
-			var page = pageName.substring(0, pageName.indexOf('.'));
+			let page = pageName.substring(0, pageName.indexOf('.'));
 			// chage URL without loading url
-			if(page.substring(0, 6) == 'plants'){
-				history.replaceState({}, 'گروه یاسمن', '?');
-			}else{
-				history.replaceState({}, 'گروه یاسمن', '?page='+page);
-			}
-
-			// Cancel Event Handler
+			history.replaceState({}, 'قالب رایگان', page.substring(0, 6) === 'plants' ? '?' : '?page=' + page);
 			return;
 		}
 
 		// Get and Set Content
 		$.get( "ajax/" + pageName, function(data) {
-  			// Button will be hidden instead of removed because there is an event listener for this button
-  			// decide whether Load More Button should be displayed or not
-
-
-  			// Remove all childs of #content except #more button
-  			$("#content > :not('#more')").remove();
+  			// Remove all children of #content except #more button
+  			$("#content > :not(#more)").remove();
   			
   			// Add data to #content before #more button
   			$(data).insertBefore("#content #more");
-  			if(pageName != 'plants-page1.html'){
-  				// this Button does not belong here
+  			if(pageName !== 'plants-page1.html'){
   				$('#content #more').css({display:'none'});
   			}else{
-  				// this Button will rock in this page
   				$('#content #more').css({display:'flex'});
   				$('.page1').animate({opacity: '1'}, 1000);
   			}  			
@@ -297,43 +254,22 @@ $(document).ready(function(){
   			// Set other (hidden) nav
   			activate.addClass('active');
   			current.removeClass('active');
-
 		});
 
-		//Scroll to top
-		$('html,body').animate({scrollTop:0}, 1000);
-
+		scrollToTop();
 		// Edit browse Url
-		var page = pageName.substring(0, pageName.indexOf('.'));
-		// chage URL without loading url
-		if(page.substring(0, 6) == 'plants'){
-			history.replaceState({}, 'گروه یاسمن', '?');
-		}else{
-			history.replaceState({}, 'گروه یاسمن', '?page='+page);
-		}
+		let page = pageName.substring(0, pageName.indexOf('.'));
+		// change URL without loading url
+		history.replaceState({}, 'قالب رایگان', page.substring(0, 6) === 'plants' ? '?' : '?page=' + page);
 	}
 
 	function lastCardSize(){
-		var $lastCard = $('#last-card');
-		if(!$lastCard){
-			return;
-		}
-		var width = $lastCard.parent().prev().find('.card:first').width();
+		const $lastCard = $('#last-card');
+		if(!$lastCard) return;
 		$lastCard.css({
-			'max-width': width
+			'max-width': $lastCard.parent().prev().find('.card:first').width()
 		});
 	}
 
-
-	function initialfunction(){
-		loadPage(true);
-		cardsContainerResize();
-		fluidContainer();
-		contentMinHeight();
-		gotTopButton();
-	}
-
-	var date = new Date();
-	var thisYear = date.getFullYear();
-	document.getElementById('this-year').innerHTML = thisYear;
+	document.getElementById('this-year').innerHTML = new Date().getFullYear();
 });
